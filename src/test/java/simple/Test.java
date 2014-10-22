43aadd31d500328e71ebb953904bde219fad18ec
Nikita <nik_krik@ukr.net>
2014-10-22 20:11:54 +0300

    first test2

---
+++ src/test/java/simple/Test.java
@@ -0,0 +1,17 @@
+package simple;
+import org.testng.annotations.*;
+import org.testng.Assert;
+
+public class Test {
+
+    @org.testng.annotations.Test
+    public void print(){
+        System.out.println("my first test");
+        Assert.assertTrue(true);
+    }
+
+
+
+
+
+}
