00fcace587122a752788b5dd695de77b1804a9f1
Nikita <nik_krik@ukr.net>
2014-10-26 14:58:23 +0200

    extract creating EmailValidator instance

--- src/test/java/homeworks/homework_03/EmailValidatorTest.java
+++ src/test/java/homeworks/homework_03/EmailValidatorTest.java
@@ -5,32 +5,29 @@ import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class EmailValidatorTest {
+    EmailValidator validator = new EmailValidator();
+
+
     @Test
     public void testValidate() throws Exception {
-        EmailValidator validator = new EmailValidator();
         Assert.assertTrue(validator.validate("yjrrdk@ya.ru"));
     }
 
     @Test
      public void testValidateFalse() throws Exception {
-        EmailValidator validator = new EmailValidator();
         Assert.assertFalse(validator.validate("yjrrdkya.ru"));
     }
 
     @Test
     @Parameters({"Email"})
     public void testValidateParameterTrue(String Email) throws Exception {
-        EmailValidator validator = new EmailValidator();
         Assert.assertTrue(validator.validate(Email));
-        System.out.println(Email);
     }
 
     @Test
     @Parameters({"WrongEmail"})
     public void testValidateParameterFalse(String WrongEmail) throws Exception {
-        EmailValidator validator = new EmailValidator();
         Assert.assertFalse(validator.validate(WrongEmail));
-        System.out.println(WrongEmail);
     }
 
 
