98d16c25405121df29b85091f92210164c47d948
Nikita <nik_krik@ukr.net>
2014-10-27 21:20:28 +0200

    add lesson practice

--- src/test/java/lessons/lesson_04/Lessontest0401.java
+++ src/test/java/lessons/lesson_04/Lessontest0401.java
@@ -1,27 +1,26 @@
 package lessons.lesson_04;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-
-import java.util.Objects;
 
 
 public class Lessontest0401 {
 
     public static WebDriver driver;
 
-    @DataProvider(name = "dataproviders")
-    public Object[][] dataproviders(){
+    @DataProvider
+    public Object[][] dataProviders(){
         return new Object[][] {
-                new Object[] {"http://testng.org"},
-                new Object[] {"http://ya.ru"},
-                new Object[] {"http://google.com"},
-                new Object[] {"http://terrasoft.ru"},
+                new Object[] {"http://testng.org/doc/index.html", "TestNG"},
+                new Object[] {"http://ya.ru", "найти"},
+                new Object[] {"http://google.com", "найти"},
+                new Object[] {"http://terrasoft.ru", "bpmonline"},
         };
     }
 
@@ -31,11 +30,14 @@ public class Lessontest0401 {
         System.out.println("initianalization");
     }
 
-    @Test(dataProvider = "dataproviders")
-    public void sometest(String site){
-        driver.get(site);
-        System.out.println("onsite: " + site);
+    @Test(dataProvider = "dataProviders")
+    public void sometest(String Url, String text){
+        driver.get(Url);
+        Assert.assertTrue(driver.findElements(By.linkText(text)).size() != 1);
+        System.out.println("onsite: " + Url + " have  - " + text);
     }
+
+
 
     @AfterMethod
     public void finalization(){
