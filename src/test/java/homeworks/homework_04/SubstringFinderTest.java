9277f534a2406b2c4388acedd1b508bb00439437
Nikita <nik_krik@ukr.net>
2014-10-28 08:46:10 +0200

    delete 1 file add data provider

--- src/test/java/homeworks/homework_04/SubstringFinderTest.java
+++ src/test/java/homeworks/homework_04/SubstringFinderTest.java
@@ -3,25 +3,43 @@ package homeworks.homework_04;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class SubstringFinderTest {
     SubstringFinder substringFinder = new SubstringFinder();
+    String mainString;
+    String subString;
 
     @BeforeMethod
     public void cleanFile(){
         StringWriterHelper.fileCleanUp("testfile.txt");
+        this.mainString = null;
+        this.subString = null;
     }
 
-    @Test
-    public void testSubstring() throws Exception {
-        Assert.assertTrue(substringFinder.substring("hello world", "ello"));
+    @Test(dataProvider = "textGenerator")
+    public void testSubstring(String mainString, String subString) throws Exception {
+        if (substringFinder.substring(mainString, subString)){
+        this.mainString = mainString;
+        this.subString = subString;
+        }
+        Assert.assertTrue(substringFinder.substring(mainString, subString));
     }
 
     @AfterMethod
     public void writeInFile(){
-        StringWriterHelper.fileWriter("testfile.txt", "hello world", "ello");
+        if (mainString != null && subString != null)
+        StringWriterHelper.fileWriter("testfile.txt", mainString, subString);
     }
+
+    @DataProvider(name = "textGenerator")
+    public Object[][] textGen(){
+        return new Object[][] {
+            new Object[] {"Hello world", "lo"},
+        };
+    }
+
 
 
 }
